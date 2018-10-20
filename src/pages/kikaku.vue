c684fb8babdd7aebcc879535388f36b82067c3f4
Nkowne63 <neutron63zf@gmail.com>
2018-10-20 21:08:30 +0900

    add: forms.kikaku's sample

---
+++ src/pages/kikaku.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  .kikaku
+    h1 this is kikaku sample
+</template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./pages/Home.vue";
+import kikaku from "./pages/kikaku";
 
 Vue.use(Router);
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "kikaku",
+      component: kikaku
     },
     {
       path: "/home",
